ff8ade8c2319fe67f4b2eb6dae73706a4ca31ac4
Fabricio <fabriciopb90@gmail.com>
2026-02-17 19:07:26 -0300

    bad solution

---
+++ src/aula/excecao/AulaExcecao.java
@@ -0,0 +1,58 @@
+
+package aula.excecao;
+
+import java.util.Locale;
+import java.util.Scanner;
+import model.entities.Account;
+
+/**
+ *
+ * @author User
+ */
+public class AulaExcecao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Informe os dados da conta");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        
+        System.out.print("Titular: ");
+        String holder = sc.nextLine();
+        
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        
+        System.out.print("Limite de saque: ");
+        double whitdrawLimit = sc.nextDouble();
+        System.out.println();
+        Account acc = new Account(number, holder, balance, whitdrawLimit);
+        
+        
+        System.out.print("Informe uma quantia para sacar: ");
+        double amount = sc.nextDouble();
+        
+        if(amount > acc.getWithdrawLimit()){
+            System.out.println("Erro de saque: A quantia excede o limite de saue");
+            
+        }
+        else if(amount > acc.getBalance()){
+            System.out.println("Erro de saque: Saldo insuficiente");
+            
+        }
+        else{
+            acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+        }
+        
+        sc.close();
+    }
+    
+}
